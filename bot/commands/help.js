8b5111f470934b3504c37ad473367e08322b34cc
ednutcode <ednutcodes@gmail.com>
2025-09-01 09:58:19 +0100

    Update help.js

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -11,11 +11,6 @@ module.exports = (bot) => {
                 return ctx.reply('❌ You are not authorized to use this bot.');
             }
 
-            const adminStatus = await new Promise(r => isAdmin(ctx.from.id, r));
-            if (!adminStatus) {
-                return ctx.reply('❌ This command is for administrators only.');
-            }
-
             const isOwner = await new Promise(r => isAdmin(ctx.from.id, r));
             
             const basicCommands = `📱 *Basic Commands*
