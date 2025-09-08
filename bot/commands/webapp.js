ed250f358f916a81cbffd68c2af66ddc738ec342
Ednut <ednut59@gmail.com>
2025-09-08 13:02:09 +0000

    Update app.js

--- bot/commands/webapp.js
+++ bot/commands/webapp.js
@@ -1,4 +1,4 @@
-// Mini App integration
+// bot/commands/webapp.js - New file for Mini App integration
 const { InlineKeyboard } = require('grammy');
 const { getUser, isAdmin } = require('../db/db');
 const crypto = require('crypto');
